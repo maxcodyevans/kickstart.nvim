b0b1288a16f7ab190d616fe3f56082f1b5abe5a6
chick <maxcodyevans@gmail.com>
2025-04-06 08:41:19 -0600

    terminal toggle

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      require('toggleterm').setup {
+        -- Default configuration
+        size = 10,
+        direction = 'horizontal', -- 'float', 'horizontal', 'vertical', 'tab'
+        shade_terminals = true,
+        float_opts = {
+          border = 'curved', -- 'single', 'double', 'shadow', 'curved'
+          width = 120,
+          height = 30,
+        },
+      }
+
+      -- Set multiple keybindings for toggling the terminal
+      vim.api.nvim_set_keymap('n', '<C-\\>', ':ToggleTerm<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '\\\\', ':ToggleTerm<CR>', { noremap = true, silent = true })
+    end,
+  },
+}
